a833e28894ae91bf0b5e2633b29d182dcc436864
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-03-30 00:31:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-test-william
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-test-william)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component'; 
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, ServerComponent, ServersComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
