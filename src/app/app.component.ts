7d8b52979c65949c039fe0f31dc839ea586dbf2d
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-04-05 14:01:04 +0800

    Updated template and styles for app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
+  // template: `<app-servers></app-servers>`,
   styleUrls: [ './app.component.css' ]
+  // styles: [`
+  //   app-servers {
+  //     color: dodgerblue;
+  //   }
+  // `]
 })
 export class AppComponent  {
   name = 'Angular';
