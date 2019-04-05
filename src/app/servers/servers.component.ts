27cc7307b3eba963b36e648abee12bb8eb134370
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-04-05 14:09:05 +0800

    Modified selectors for app-servers (by element, attribute, and class) and changed template for app-servers

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // By HTML element
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  
+  // By HTML attribute
+  // selector: '[app-servers]',
+  
+  // By HTML class
+  // selector: '.app-servers',
+  
+  // templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
